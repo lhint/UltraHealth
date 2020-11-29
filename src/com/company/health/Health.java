9ddfb5921ce66b41fbb7d7cd53ab550396e49e71
Luke Hinton <lukejohnhinton@gmail.com>
2020-11-29 14:24:18 +0000

    Health Class Created.
    Health Class created.

--- src/com/company/health/Health.java
+++ src/com/company/health/Health.java
@@ -2,10 +2,46 @@ package com.company.health;
 
 /**
  * Created by Luke Hinton on 29/11/2020
- * UPDATE COMMENTS ABOUT PROGRAM HERE
+ * Main health class for the Ultra Health App.
  **/
+
 public class Health
 {
-   
+   public String personName;
+   public int personAge;
+
+   public Health()
+   {
+
+   }//Default constructor
+
+   public Health(String name, int age)
+   {
+      personName = name;
+      personAge = age;
+   }///Additional constructor
+
+   //Setters
+   public void setPersonName(String name)
+   {
+      personName = name;
+   }
+
+   public void setPersonAge(int age)
+   {
+      personAge = age;
+   }
+
+   //Getters
+
+   public String getPersonName()
+   {
+      return personName;
+   }
+
+   public int getPersonAge()
+   {
+      return personAge;
+   }
 
 }//class
