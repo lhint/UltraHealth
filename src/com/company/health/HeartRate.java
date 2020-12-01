c49dcb8b1b6fcebed26d7e88a3d34078c49bce46
Luke Hinton <lukejohnhinton@gmail.com>
2020-12-01 15:32:26 +0000

    Updated HeartRate.java
    Updated HeartRate.java and hopefully should be finished.

--- src/com/company/health/HeartRate.java
+++ src/com/company/health/HeartRate.java
@@ -1,172 +1,73 @@
 package com.company.health;
-import java.util.Scanner;
 
 /**
  * Created by Luke Hinton on 29/11/2020
- * HeartRate sub class of the main class Health. This class will provide the code for the heart rate function of the app.
+ * HeartRate sub class of the main class Health. This class will provide the code for the heart rate function of the app. Take the persons age away from 220
+ * to find average max heart rate.
  **/
 public class HeartRate extends Health
 {
 
-   public final int MAXHEARTRATE = 220;
-   public int currentAverage, enteredHeartRate;
+   public int currentAverage;
 
    public HeartRate()
    {
 
    }//Default constructor
 
-   public HeartRate(String name, int age, int heartRate)
+   public HeartRate(String name, int age)
    {
       super(name, age);
-      enteredHeartRate = heartRate;
    }//Alternative constructor
-
-   //Setters
-   public void setEnteredHeartRate(int heartRate)
-   {
-      enteredHeartRate = heartRate;
-   }
-
-   //Getters
-   public int getEnteredHeartRate()
-   {
-      return enteredHeartRate;
-   }
 
    //Method to calculate average age heart rate.
 
-   public int calculateHeartRate(int age, int heartRate)
+   public int calculateHeartRate(int age)
    {
-      Scanner keyboard = new Scanner(System.in);
-      System.out.print("Please enter heart rate over one minute.\nTo do this find your pulse and count it over the course of one minute and enter the total pulses:\nHeart rate: ");
-      setEnteredHeartRate(keyboard.nextInt());
+      final int MAXHEARTRATE = 220;
       currentAverage = MAXHEARTRATE - age;
       return currentAverage;
    }//calculateHeartRate
 
-   //IsNormal
-
-   public String isNormal(boolean isNormal)
-   {
-      if(isNormal)
-      {
-         return ("is ");
-      } else
-      {
-         return ("is not ");
-      }
-   }//isNormal
-
    //Method to check if the heart rate is normal for age.
 
-   public String checkIsNormal(int age, int heartRate)
+   public String checkIsNormal(int age)
    {
       boolean isNormal;
 
-        if(age >= 0 && age < 30)
+      if (age >= 0 && age < 30)
       {
-         if(heartRate >= 100 && age <= 170)
-         {
-            isNormal = true;
-         } else
-         {
-            isNormal = false;
-         }
-         return ("Heart rate should be between 100 - 170bpm" + isNormal(isNormal) + " normal");
-      } else if(age >= 30 && age < 35)
+         return (" and should be between 190 - 200bpm for your age.");
+      } else if (age >= 30 && age < 35)
       {
-         if(heartRate >= 95 && age <= 162)
-         {
-            isNormal = true;
-         } else
-         {
-            isNormal = false;
-         }
-         return ("Heart rate should be between 95 - 162"  + isNormal(isNormal) + " normal");
-      } else if(age >= 35 && age < 40)
+         return (" and should be between 185 - 190 for your age");
+      } else if (age >= 35 && age < 40)
       {
-         if(heartRate >= 93 && age <= 157)
-         {
-            isNormal = true;
-         } else
-         {
-            isNormal = false;
-         }
-         return ("Heart rate should be between 93 - 157" + isNormal(isNormal) + " normal");
-      } else if(age >= 40 && age < 45)
+         return (" and should be between 180 - 185 for your age");
+      } else if (age >= 40 && age < 45)
       {
-         if(heartRate >= 90 && age <= 153)
-         {
-            isNormal = true;
-         } else
-         {
-            isNormal = false;
-         }
-           return ("Heart rate should be between 90 - 153" + isNormal(isNormal) + " normal");
-      } else if(age >= 45 && age < 50)
+         return (" and should be between 175 - 180 for your age");
+      } else if (age >= 45 && age < 50)
       {
-         if(heartRate >= 88 && age <= 149)
-         {
-            isNormal = true;
-         } else
-         {
-            isNormal = false;
-         }
-           return ("Heart rate should be between 88 - 149" + isNormal(isNormal) + " normal");
-      } else if(age >= 50 && age < 55)
+         return (" and should be 170 - 185 for your age");
+      } else if (age >= 50 && age < 55)
       {
-         if(heartRate >= 85 && age <= 145)
-         {
-            isNormal = true;
-         } else
-         {
-            isNormal = false;
-         }
-           return ("Heart rate should be between 85 - 145" + isNormal(isNormal) + " normal");
-      } else if(age >= 55 && age < 60)
+         return (" and should be between 165 - 170 for your age");
+      } else if (age >= 55 && age < 60)
       {
-         if(heartRate >= 83 && age <= 140)
-         {
-            isNormal = true;
-         } else
-         {
-            isNormal = false;
-         }
-           return ("Heart rate should be between 83 - 140" + isNormal(isNormal) + " normal");
-      } else if(age >= 60 && age < 65)
+         return (" and should be between 160 - 165 for your age");
+      } else if (age >= 60 && age < 65)
       {
-         if(heartRate >= 80 && age <= 136)
-         {
-            isNormal = true;
-         } else
-         {
-            isNormal = false;
-         }
-           return ("Heart rate should be between 80 - 136" + isNormal(isNormal) + " normal");
-      } else if(age >= 65 && age < 70)
+         return (" and should be between 155 - 160 for your age");
+      } else if (age >= 65 && age < 70)
       {
-         if(heartRate >= 78 && age <= 132)
-         {
-            isNormal = true;
-         } else
-         {
-            isNormal = false;
-         }
-           return ("Heart rate should be between 78 - 132" + isNormal(isNormal) + " normal");
-      } else if( age >= 70)
+         return (" and should be between 150 - 155 for your age");
+      } else if (age >= 70)
       {
-         if(heartRate >= 75 && age <= 128)
-         {
-            isNormal = true;
-         } else
-         {
-            isNormal = false;
-         }
-           return ("Heart rate should be between 75 - 128" + isNormal(isNormal) + " normal");
+           return (" and should be between 145 - 150 for your age");
       } else
          {
-           return ("Please enter a valid age!");
+           return ("please enter a valid age!");
          }
    }//checkIsNormal
 
@@ -174,7 +75,7 @@ public class HeartRate extends Health
 
    public String toString()
    {
-      return checkIsNormal(personAge, enteredHeartRate);
+      return (personName + ", your max heart rate is " + calculateHeartRate(personAge) + checkIsNormal(personAge));
 
    }//toString
 
